76f35146ca0f427412cc6b8e8676ffcfd7c2bd82
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-20 14:43:13 +0200

    Correzione logica javascript

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
 
 var numeri = [];
 
-for (var i = 0; i < 100; i++) {
+for (var i = 1; i < 101; i++) {
   if ( (i % 3 === 0) && (i % 5 === 0) ) {
     numeri.push('FizzBuzz');
   }
